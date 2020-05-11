1fa4eca1b5ce5c927d1c97de74d365bc2a2a5c15
sandrah <morandesandy@gmail.com>
2020-05-11 12:47:52 -0500

    api routes

--- models/restaurant.js
+++ models/restaurant.js
@@ -1,15 +1,22 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const ObjectId = mongoose.Types.ObjectId;
+
 
 const restaurantSchema = new Schema({
+ 
   restName: { type: String, required: true },
   location: { type: String, required: true },
   websiteURL: { type: String, required: true },
   photos: { type: String, required: true },
   notes:{
-    type: Schema.Types.ObjectID,
-    ref: "Notes"
+    type: Schema.Types.ObjectId,
+    ref: "notes"
 },
+ owner: {
+    type: Schema.Types.ObjectId,
+    ref: "User"
+  },
 });
 
 const Restaurant = mongoose.model("Restaurant", restaurantSchema);
