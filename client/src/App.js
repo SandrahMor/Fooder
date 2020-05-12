fc64f20cd92b626212c966caed36d89ea5eafc61
sandrah <morandesandy@gmail.com>
2020-05-12 13:55:19 -0500

    added nav bar and links to pages

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./pages/home";
 import Saved from "./pages/saved";
+import Nav from "./components/Nav";
 
 
 function App() {
   return (
     <Router>
       <div>
+       <Nav />
         <Switch>
           <Route exact path="/" component={Home} />
           <Route exact path="/saved" component={Saved} />
