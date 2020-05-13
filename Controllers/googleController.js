23afabcc889554a0739c7bd79a6cb3a3183dcdb6
sandrah <morandesandy@gmail.com>
2020-05-13 14:23:34 -0500

    google router

--- Controllers/googleController.js
+++ Controllers/googleController.js
@@ -1,30 +1,35 @@
 const axios = require("axios");
-const db = require("../models");
+// const db = require("../models");
 
 
 module.exports = {
     findAll: function(req, res) {
-      const { query: params } = req;
-      console.log(req)
+      // const { query: params } = req;
+    
       axios
         .get("https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=AIzaSyBVbcko5gMzL50MmzOJnfAk86hsIJVvu6A&location=45.086140,-93.258360&radius=24000&type=restaurant") 
-        .then(results =>
-          results.data.items.filter(
-            result =>
-              result.name &&
-              result.opening_hours.open_now
-          )
-        )
-        .then(apiRestaurants =>{
-          console.log(apiRestaurants)
-          db.Restaurant.find().then(dbRestaurants =>
-            apiRestaurants.filter(apiRestaurant =>
-              dbRestaurants.every(dbRestaurant => dbRestaurant.googleId.toString() !== apiRestaurant.id)
-            )
-          )
-        
-        .then(restaurants => res.json(restaurants))
+        console.log(res)
+        .then(res => (res.json))   
+             
         .catch(err => res.status(422).json(err));
-    
-        });
-      }}
+    }
+  }
+        // .then(results =>
+        //   results.data.items.filter(
+        //     result =>
+        //       result.name &&
+        //       result.opening_hours.open_now
+        //   )
+        // )
+        // .then(apiRestaurants =>{
+        //   db.Restaurant.find().then(dbRestaurants =>
+        //     apiRestaurants.filter(apiRestaurant =>
+        //       dbRestaurants.every(dbRestaurant => dbRestaurant.googleId.toString() !== apiRestaurant.id)
+        //     )
+        //   )
+        
+        // .then(restaurants => res.json(restaurants))
+  
+
+
+
--- routes/api/index.js
+++ routes/api/index.js
@@ -2,6 +2,7 @@ const path = require("path");
 const router = require("express").Router();
 const userRoutes = require("./user");
 const restRoutes = require("./restaurant");
+const googleRoutes = require("./google");
 
 // user routes
 router.use("/", userRoutes);
@@ -10,8 +11,8 @@ router.use("/", userRoutes);
 // router.use("/:id/restaurant", restRoutes);
 
 router.use("/restaurant", restRoutes)
-// note Routes
-// router.use("/note", noteRoutes);
+
+router.use("/google", googleRoutes)
 
 
 module.exports = router;
