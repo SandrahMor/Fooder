e912005d69c1a2914fb443938bcba09969880725
sandrah <morandesandy@gmail.com>
2020-05-14 14:28:50 -0500

    heroku deployment

--- server.js
+++ server.js
@@ -17,7 +17,8 @@ if (process.env.NODE_ENV === "production") {
   }
   app.use(routes);
 
-mongoose.connect('mongodb://127.0.0.1:27017/fooder', { useNewUrlParser: true });
+var MONGODB_URI = process.env.MONGODB_URI ||'mongodb://127.0.0.1:27017/fooder'
+mongoose.connect(MONGODB_URI);
 const connection = mongoose.connection;
 connection.once('open', function() {
     console.log("MongoDB database connection established successfully");
