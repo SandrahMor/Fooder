96df100e74cb61942d3c8d251afc1629b9863f44
sandrah <morandesandy@gmail.com>
2020-05-14 15:29:01 -0500

    heroku deployment

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 const routes = require("./routes");
 
 
@@ -12,10 +12,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(bodyParser.json());
 
-// if (process.env.NODE_ENV === "production") {
-//     app.use(express.static("client/build"));
-//   }
-//   app.use(routes);
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+  }
+  app.use(routes);
 
 var MONGODB_URI = process.env.MONGODB_URI ||'mongodb://127.0.0.1:27017/fooder'
 mongoose.connect(MONGODB_URI);
